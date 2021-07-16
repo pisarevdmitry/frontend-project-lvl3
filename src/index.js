49f313f326a9949d6782357b6c723aa2a175f538
dmitry <pisarev_dmitry@mail.ru>
2021-07-16 15:52:59 +0300

    change main

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/modal';
 import app from './app/app';
 
-const init = () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    app();
-  });
-};
-init();
-
-export default app;
+app();
