28a3f8a07a5279a6420d3ca8800b2e2cd9509ce8
dmitry <pisarev_dmitry@mail.ru>
2021-07-16 11:25:05 +0300

    move index.html

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html',
+      template: './index.html',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles.css',
