23e8d918d956909946f1dda4df18ceccce2c325d
dmitry <pisarev_dmitry@mail.ru>
2021-07-16 12:22:10 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/modal';
 import app from './app/app';
 
-app();
+const init = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    app();
+  });
+};
+init();
 
 export default app;
